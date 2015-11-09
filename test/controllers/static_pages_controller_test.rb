ec676d2d442ddada0b6f5e0d53328427692a4cc0
kevm254 <kevm2@yahoo.com>
2015-11-09 04:00:08 -0600

    new commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,9 +11,8 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
+  test "should get about" do
+    get :about
     assert_response :success
   end
-
 end
