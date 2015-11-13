84acfc6b61a5390ab2e9573c7ff1b06f874f4bf7
kevm254 <kevm2@yahoo.com>
2015-11-12 20:56:58 -0600

    Improved email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,7 +38,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid email addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[foo@bar..com user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -50,10 +50,18 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+    assert_not duplicate_user.valid?
   end
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
