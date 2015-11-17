23bc5699e36905da4a57b163083c3fd618db3953
kevm254 <kevm2@yahoo.com>
2015-11-17 14:44:35 -0600

    Logging out and in added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  # navigate to main site areas
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
 
+  # signup
   get 'signup' => 'users#new'
+
+  # login sessions
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  # users
   resources :users
 end
