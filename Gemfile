ce1dc4f0130862eac16cd7d0a5933a4deda15547
kevm254 <kevm2@yahoo.com>
2015-11-19 00:14:25 -0600

    refactoring user template

--- Gemfile
+++ Gemfile
@@ -4,6 +4,12 @@ source 'https://rubygems.org'
 gem 'bootstrap-sass', '3.2.0.0'
 # Use bcrypt
 gem 'bcrypt', '3.1.7'
+# Add Faker gem
+gem 'faker', '1.4.2'
+# add pagination gems
+gem 'will_paginate', '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -26,8 +31,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if
-      @user.update_attributes(user_params)
+    if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
@@ -44,8 +48,15 @@ class UsersController < ApplicationController
   # Confirms a logged-in user
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
