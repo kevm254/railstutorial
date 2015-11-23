b7d3ad52fb5121dfa9115edb8398b515d362eee3
kevm254 <kevm2@yahoo.com>
2015-11-22 22:15:18 -0600

    Added microposts

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,6 @@ Rails.application.routes.draw do
   # users
   resources :users
   resources :account_activations, only: [:edit]
-
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 end
