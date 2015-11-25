5edc35f2f24029888199874724c330add67a79d9
kevm254 <kevm2@yahoo.com>
2015-11-25 00:28:57 -0600

    more error corrections for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,5 +23,5 @@ users = User.all
 user = users.first
 following = users[2..50]
 followers = users[3..40]
-following.each { |followed| user.follow(followed)}
-followers.each { |follower| follower.follow}
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
